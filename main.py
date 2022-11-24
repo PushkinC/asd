098b8259e5390875d7061038e2a59f3e09618b5a
PushkinC <zolotarev_d_e@mail.ru>
2022-11-24 03:24:39 +0300

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import math
+import sys
+import random as rnd
+from PyQt5 import QtGui, uic
+from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtGui import QPainter, QColor, QPolygon
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
 
 
-# Press the green button in the gutter to run the script.
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pos = (400, 300)
+        self.circle = False
+        self.pushButton.clicked.connect(self.cl)
+
+    def cl(self):
+        self.size = rnd.randint(50, 100)
+        self.circle = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.circle:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_points(qp)
+            qp.end()
+
+
+    def draw_points(self, qp):
+        pen = QtGui.QBrush(QtGui.QColor('yellow'))
+        qp.setBrush(pen)
+        qp.drawEllipse(self.pos[0] - self.size // 2, self.pos[1] - self.size // 2, self.size, self.size)
+
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
